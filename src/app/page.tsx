1165e18ff70414c917f82c9a88ce5f603f3387f1
Santiago Servera <santiagoservera21@gmail.com>
2024-03-12 23:42:52 -0300

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import LayoutEcommerce from "@/layout/LayoutEcommerce";
 import Image from "next/image";
-
+import { Products } from "@/components/Products";
 export default function Home() {
   return (
     <LayoutEcommerce>
 
      
      <div className="bg-ecommerce">
+
+      <Products/>
 
      </div>
 
---
+++ src/components/Products.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+
+import fetchArticles from '@/services/Articles';
+import React from 'react'
+import { useEffect, useState } from 'react';
+
+
+
+
+export const Products = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await fetchArticles();
+          setArticles(data);
+        } catch (error) {
+          // Manejar el error si es necesario
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Lista de Artículos</h1>
+        <ul>
+          {articles.map((article:any) => (
+            <li key={article.id}>{article.nombre}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
