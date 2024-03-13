3d37d1a86e7e8c8ac1b30b5a5714c87a30e649a9
Santiago Servera <santiagoservera21@gmail.com>
2024-03-12 23:46:50 -0300

    pretier

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,38 +1,33 @@
-"use client"
-
+'use client';
 
 import fetchArticles from '@/services/Articles';
-import React from 'react'
+import React from 'react';
 import { useEffect, useState } from 'react';
 
-
-
-
 export const Products = () => {
-    const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const data = await fetchArticles();
-          setArticles(data);
-        } catch (error) {
-          // Manejar el error si es necesario
-        }
-      };
-  
-      fetchData();
-    }, []);
-  
-    return (
-      <div>
-        <h1>Lista de Artículos</h1>
-        <ul>
-          {articles.map((article:any) => (
-            <li key={article.id}>{article.nombre}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchArticles();
+        setArticles(data);
+      } catch (error) {
+        // Manejar el error si es necesario
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Artículos</h1>
+      <ul>
+        {articles.map((article: any) => (
+          <li key={article.id}>{article.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
