911ad6f2d399d86d110fb5709d074d4867ffcffc
AgusPaez <agus.123.paez@gmail.com>
2024-03-15 18:57:31 -0300

    edit imports

--- src/app/profile/account/page.tsx
+++ src/app/profile/account/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 //Components
 
-import { Account } from '@/components/Account';
+import { Account } from '@/components/profile/Account';
 import { Navbar } from '@/components/Navbar';
 import { AsideProfile } from '@/components/profile/AsideProfile';
 
--- src/app/profile/cards/page.tsx
+++ src/app/profile/cards/page.tsx
@@ -1,6 +1,6 @@
 //Components
 
-import { Account } from '@/components/Account';
+import { Account } from '@/components/profile/Account';
 import { Navbar } from '@/components/Navbar';
 import { AsideProfile } from '@/components/profile/AsideProfile';
 
