490d3963e59287716f37716efa90f34215aa8a03
JuanCruz0715 <juanchi97cruz@gmail.com>
2024-03-17 20:56:03 -0300

    login validator more alert

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -2,36 +2,34 @@
 import React, { useState } from 'react';
 import { useAuth } from '../../context/Auth';
 import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
 
 export const Login = () => {
-  const { login, dataLogin } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { login } = useAuth();
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
-  const router = useRouter();
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    try {
-      await login({ username, password });
+  const handleLoginSubmit = async (data: any) => {
+        try {
+      await login({ username: data.username, password: data.password });
     } catch (error) {
       console.log(error);
     }
   };
-
-  if (dataLogin?.userLogin) {
-    router.push('/');
-  }
 
   return (
     <div className="w-full h-[442px] flex justify-center items-center relative">
       <h1 className="absolute top-[20%] font-bold text-4xl md:hidden">LOGIN</h1>
       <div className="w-full h-full flex justify-center items-center">
         <form
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(handleLoginSubmit)}
           className="flex flex-col items-center w-full md:w-[75%] gap-7 md:gap-6"
         >
-          <div className="flex items-center gap-2 border-black border-b-[1px]">
+          <div className="relative flex items-center gap-2 border-black border-b-[1px]">
             <img
               src="/userIconLogin.png"
               alt="user-icon"
@@ -39,14 +37,18 @@ export const Login = () => {
             />
             {/* username */}
             <input
+            {...register('username', { required: true })}
               type="text"
               placeholder="Username"
               className=" outline-none w-[250px] sm:w-[400px] md:w-[300px]"
-              value={username}
-              onChange={(e) => setUserName(e.target.value)}
-            />
+                          />
+             {errors.username && (
+              <span className="absolute text-red-500 text-xs bottom-0 right-0">
+                Username is required
+              </span>
+            )}
           </div>
-          <div className="flex items-center gap-2 border-black border-b-[1px]">
+          <div className="relative flex items-center gap-2 border-black border-b-[1px]">
             <img
               src="/passwordIconLogin.png"
               alt="password-icon"
@@ -54,12 +56,13 @@ export const Login = () => {
             />
             {/* password */}
             <input
+            {...register('password', { required: true })}
               type="password"
               placeholder="Password"
               className=" outline-none w-[250px] sm:w-[400px] md:w-[300px]"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+                          />
+             {errors.password && <span className="absolute text-red-500 text-xs bottom-0 right-0"
+            >Password is required</span>}
           </div>
           <div className="flex justify-between items-center w-full">
             <a className="mr-4 font-bold text-xs cursor-pointer">
